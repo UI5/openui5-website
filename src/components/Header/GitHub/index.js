e68ffbd4789c04b896f8822c961d874541b48f59
Fabian Tempel <fabian.tempel@sap.com>
2025-10-10 08:12:52 +0200

    got rid of all gatsby and react stuff
    moved event and person data to new location and optimized images.

--- src/components/Header/GitHub/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./styles.module.css";
-
-const GitHub = () => (
-    <a href="https://github.com/SAP/openui5" rel="noopener noreferrer" target="_blank" className={styles.GitHub}>GitHub</a>
-);
-
-export default GitHub;
